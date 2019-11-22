70c882b86febc85f858b8d2f6b34a17bbcd9c057
KUNAL SAHU <53874235+mrkunalsahu@users.noreply.github.com>
2019-11-22 14:14:32 +0530

    Add files via upload

---
+++ VID11_Dynamic_Allocation.cpp
@@ -0,0 +1,16 @@
+#include<iostream>
+#include<vector>
+using namespace std;
+int main()
+{
+	int n;
+	cout<<"Enter the size : ";
+
+	cin>>n;
+int *ar= new int[n];
+cout<<endl;
+cout<<sizeof(ar);
+
+
+	return 0;
+}
